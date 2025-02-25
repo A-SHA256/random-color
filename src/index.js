5edf7fa665ff620995e5cff92325c58071c491c3
A-SHA256 <arsanshataev0496@gmail.com>
2025-02-25 11:16:49 +0100

    add random color app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('Hello, world!');
+import {chars} from './chars.js';
+
+
+// Create div for hexadecimal text incicator
+const hexContainer = document.createElement('div');
+hexContainer.classList.add('container');
+document.body.append(hexContainer);
+
+// Create button for random background color change 
+const changeColorCont = document.createElement('div');
+changeColorCont.classList.add('text-container');
+hexContainer.append(changeColorCont);
+const changeColorBtn = document.createElement('button');
+changeColorBtn.classList.add('color-pick-button');
+hexContainer.append(changeColorBtn);
+
+
+
+//Create function for generating hex string
+function colorChange() {
+    let hexString = '#';
+    for (let i = 0; i < 6; i++) {
+        const valueGenerator = chars[Math.floor(Math.random()*16)];
+        hexString += valueGenerator;
+        console.log(hexString);
+    }
+    changeColorCont.innerText = hexString;
+    document.body.style.backgroundColor = hexString;
+}
+
+changeColorBtn.addEventListener('click', ()=>{
+    colorChange();
+})
